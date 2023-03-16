1ac299ca7d9a3313b9c5fa3011beb3a52db2acea
Yelena Cox <yelenacox@gmail.com>
2023-03-16 17:28:02 -0500

    added app.yaml and <tbody> to App.js

--- src/App.js
+++ src/App.js
@@ -5,36 +5,38 @@ export const App = () => {
 
   const [data, setData] = useState([])
 
-  return <div>Hello World!</div>
-  // const getData = async () => {
-  //   await fetch(`https://anvil-fhir-vumc.uc.r.appspot.com/fhir/ResearchStudy`)
-  //     .then(res => res.json())
-  //     .then((d) => {
-  //       setData(d)
-  //     })
-  // }
 
-  // useEffect(() => {
-  //   getData()
-  // }, []
-  // )
+  const getData = async () => {
+    await fetch(`https://anvil-fhir-vumc.uc.r.appspot.com/fhir/ResearchStudy`)
+      .then(res => res.json())
+      .then((d) => {
+        setData(d)
+      })
+  }
 
-  // return <>
-  //   <div className="data-table">
-  //     <table>
-  //       <tr>
-  //         <th>URL</th>
-  //         <th>Title</th>
-  //       </tr>
-  //       {data.entry?.slice(0, 10).map(
-  //         (d, index) => {
-  //           return <tr key={index} className="table-cell">
-  //             <td className='url-div'>{d.fullUrl}</td>
-  //             <td className='title-div'>{d.resource?.title}</td>
-  //           </tr>
-  //         }
-  //       )}
-  //     </table>
-  //   </div>
-  // </>
+  useEffect(() => {
+    getData()
+  }, []
+  )
+
+  return <>
+    <div className="data-table">
+      <table>
+        <tbody>
+          <tr>
+            <th>URL</th>
+            <th>Title</th>
+          </tr>
+        </tbody>
+        {data.entry?.slice(0, 10).map(
+          (d, index) => {
+            return <tr key={index} className="table-cell">
+              <td className='url-div'>{d.fullUrl}</td>
+              <td className='title-div'>{d.resource?.title}</td>
+            </tr>
+          }
+        )}
+      </table>
+    </div>
+  </>
 }
