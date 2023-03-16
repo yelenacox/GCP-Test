cc8f28a85da25d5361f6b9d0731a37f5a0d23e90
Yelena Cox <yelenacox@gmail.com>
2023-03-16 11:50:35 -0500

    trying to fix error

--- src/App.js
+++ src/App.js
@@ -5,35 +5,36 @@ export const App = () => {
 
   const [data, setData] = useState([])
 
-  const getData = async () => {
-    await fetch(`https://anvil-fhir-vumc.uc.r.appspot.com/fhir/ResearchStudy`)
-      .then(res => res.json())
-      .then((d) => {
-        setData(d)
-      })
-  }
+  return <div>Hello World!</div>
+  // const getData = async () => {
+  //   await fetch(`https://anvil-fhir-vumc.uc.r.appspot.com/fhir/ResearchStudy`)
+  //     .then(res => res.json())
+  //     .then((d) => {
+  //       setData(d)
+  //     })
+  // }
 
-  useEffect(() => {
-    getData()
-  }, []
-  )
+  // useEffect(() => {
+  //   getData()
+  // }, []
+  // )
 
-  return <>
-    <div className="data-table">
-      <table>
-        <tr>
-          <th>URL</th>
-          <th>Title</th>
-        </tr>
-        {data.entry?.slice(0, 10).map(
-          (d, index) => {
-            return <tr key={index} className="table-cell">
-              <td className='url-div'>{d.fullUrl}</td>
-              <td className='title-div'>{d.resource?.title}</td>
-            </tr>
-          }
-        )}
-      </table>
-    </div>
-  </>
+  // return <>
+  //   <div className="data-table">
+  //     <table>
+  //       <tr>
+  //         <th>URL</th>
+  //         <th>Title</th>
+  //       </tr>
+  //       {data.entry?.slice(0, 10).map(
+  //         (d, index) => {
+  //           return <tr key={index} className="table-cell">
+  //             <td className='url-div'>{d.fullUrl}</td>
+  //             <td className='title-div'>{d.resource?.title}</td>
+  //           </tr>
+  //         }
+  //       )}
+  //     </table>
+  //   </div>
+  // </>
 }
